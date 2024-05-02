a4a4835728efdc6b0d90e4070375157572a3975e
Alexander Poselentsev <as.poselentsev@gmail.com>
2024-05-03 01:30:34 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div className='food'>{counter <= 4 ? 'Еда' : 'Много еды'}</div>
+      <div className="buttons">
+        <button onClick={() => {
+          if (counter > 0) setCounter(counter - 1)
+        }}>{"<"}</button>
+        <div className="counter">Количество: {counter}</div>
+        <button onClick={() => setCounter(counter + 1)}>{">"}</button>
+      </div>
     </div>
-  );
+   )
 }
-
+ 
 export default App;
