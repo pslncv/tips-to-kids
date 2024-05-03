ed5aeeb972e12c1fdc32be7d87332a46d752ef0a
Alexander Poselentsev <as.poselentsev@gmail.com>
2024-05-04 01:33:40 +0300

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
+import { recipes } from '../src/data/recipes';
 import Product from './components/product';
 
 const App = () => {
   return (
     <div className='wrapper'>
         <div className='product__list'>
-            <Product />
-            <Product />
-            <Product />
-            <Product />
+            <Product product={recipes}/>
         </div>
     </div>
    )
--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,17 +1,27 @@
 import React, {useState} from 'react';
 
-const Product = () => {
+const Product = (props: any) => {
+    console.log(props);
+    
     const [counter, setCounter] = useState(0)
     return (
         <div className='product__item'>
-            <div className='product__title'>{counter <= 4 ? 'Еда' : 'Много еды'}</div>
-                <div className="buttons">
-                    <button onClick={() => {
-                        if (counter > 0) setCounter(counter - 1)
-                    }}>{"<"}</button>
-                    <div className="counter">Количество: {counter}
-                </div>
-                <button onClick={() => setCounter(counter + 1)}>{">"}</button>
+            <div className='product__container'>
+                <img src={props.product[0].image} alt={props.product[0].name} className='product__image'/>
+            </div>
+            <span className='product__title'>
+                {props.product[0].name}
+            </span>
+            <span className='product__price'>
+                $12
+            </span>
+            <div className="buttons">
+                <button onClick={() => {
+                    if (counter > 0) setCounter(counter - 1)
+                }}>{"<"}</button>
+                <div className="counter">Counter: {counter}
+            </div>
+            <button onClick={() => setCounter(counter + 1)}>{">"}</button>
             </div>
         </div>
     );
